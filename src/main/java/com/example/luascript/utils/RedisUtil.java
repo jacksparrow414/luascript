db9ce507bb4b49c1a21d2947d352a84b7e4f68f6
duhongbo <hongbo.du@etix.com>
2021-06-29 14:06:15 +0800

    test redis call luascript

--- src/main/java/com/example/luascript/utils/RedisUtil.java
+++ src/main/java/com/example/luascript/utils/RedisUtil.java
@@ -1,10 +1,8 @@
 package com.example.luascript.utils;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.script.DefaultRedisScript;
 import org.springframework.scripting.support.ResourceScriptSource;
@@ -13,6 +11,7 @@ import org.springframework.stereotype.Component;
 import java.util.Collections;
 
 @Component
+@Slf4j
 public class RedisUtil {
 
     @Autowired
@@ -22,6 +21,7 @@ public class RedisUtil {
         DefaultRedisScript script = new DefaultRedisScript();
         script.setScriptSource(new ResourceScriptSource(new ClassPathResource("/luascripts/" + luaScriptName + ".lua")));
         script.setResultType(String.class);
-        redisTemplate.execute(script, Collections.singletonList(key), value);
+        String execute = (String) redisTemplate.execute(script, Collections.singletonList(key), value);
+        log.info(execute);
     }
 }
